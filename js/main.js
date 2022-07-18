f09a33fd6cd7d99fe8a163dadc61ff93e521ec4b
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-07-18 14:44:22 +0200

    add: dichiarato un array di oggetti contenente le informazioni del team + scritto ciclo for per scorrere tra gli elementi dell'array e stampare in console chiavi e valori degli oggetti contenuti + ciclo for per stampare  nel DOM le chiavi e i valori degli oggetti

---
+++ js/main.js
@@ -0,0 +1,59 @@
+const team =    [
+                    {
+                        name: 'Wayne Barnett',
+                        role: 'Founder & CEO',
+                        photo: 'wayne-barnett-founder-ceo.jpg'
+                    },
+
+                    {
+                        name: 'Angela Caroll',
+                        role: 'Chief Editor',
+                        photo: 'angela-caroll-chief-editor.jpg'
+                    },
+
+                    {
+                        name: 'Walter Gordon',
+                        role: 'Office Manager',
+                        photo: 'walter-gordon-office-manager.jpg'
+                    },
+
+                    {
+                        name: 'Angela Lopez',
+                        role: 'Social Media Manager',
+                        photo: 'angela-lopez-social-media-manager.jpg'
+                    },
+
+                    {
+                        name: 'Scott Estrada',
+                        role: 'Developer',
+                        photo: 'scott-estrada-developer.jpg'
+                    },
+
+                    {
+                        name: 'Barbara Ramos',
+                        role: 'Graphic Designer',
+                        photo: 'barbara-ramos-graphic-designer.jpg'
+                    }
+                ];
+
+let wrapperDom = document.getElementById('wrapper');
+
+for (let key in team) {  // ciclo per scorrere tra gli oggetti dell'array
+    console.log(team[key]); // output = Object {key: 'valore', ecc..}
+}
+
+for (let i = 0; i < team.length; i++) {
+    let info = document.createElement('div');
+    info.classList.add('info-box');
+
+    let nameBox = document.createElement('h1');
+    let roleBox = document.createElement('h2');
+    let photoBox = document.createElement('h4');
+    
+    nameBox.append(team[i].name);
+    roleBox.append(team[i].role);
+    photoBox.append(team[i].photo);
+
+    info.append(nameBox, roleBox, photoBox);
+    wrapperDom.append(info);
+}
